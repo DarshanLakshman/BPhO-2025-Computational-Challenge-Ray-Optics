f3a405930f390067a1d3b8cad792e7f3c3ec8eb6
Darshan Lakshman <147950435+DarshanLakshman@users.noreply.github.com>
2025-08-08 11:14:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import dash
 external_css = ["https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css", ]
 
 app = Dash(__name__, pages_folder='pages', use_pages=True, external_stylesheets=external_css)
+server= app.server
 
 img_tag = html.Img(src="assets/cc.png", width=27, className="m-1")
 pages_links = [dcc.Link(page['name'], href=page["relative_path"], className="nav-link")\
@@ -24,4 +25,4 @@ app.layout = html.Div([
 ], style={"height": "1000vh", "background-color": "#e3f2fd"})
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run_server(debug=True)
