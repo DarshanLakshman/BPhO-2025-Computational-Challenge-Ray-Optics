af80db20f3caecb708fc5b2ec069ac62dc4af45e
Darshan Lakshman <147950435+DarshanLakshman@users.noreply.github.com>
2025-08-08 12:41:30 +0100

    Update Challenge 5.py

--- pages/Challenge 5.py
+++ pages/Challenge 5.py
@@ -4,69 +4,58 @@ import numpy as np
 from PIL import Image
 import plotly.graph_objects as go
 
-def challenge_5_code(offset_x, offset_y, step=10):
-    img = Image.open("cat.jpeg").convert("RGB").resize((50, 50))
-    data = np.array(img)
-    h, w, d = data.shape
+img = Image.open("cat.jepg").convert("RGB").resize((50, 50))
+data = np.array(img)
+h, w, _ = data.shape
+yy, xx = np.mgrid[0:h, 0:w]
+xs = xx.flatten()
+ys = h - yy.flatten() 
+colors = data.reshape(-1, 3)
+color_strings = [f'rgb({r},{g},{b})' for r, g, b in colors]
 
-    # Bottom-left corner of original image at x = 10
-    initial_x_offset = 10
+step = 10
+margin_from_axis = 10  
 
-    yy, xx = np.mgrid[0:h, 0:w]
-    xs = xx.flatten()
-    ys = yy.flatten()
-
-    # Flip y axis so bottom-left is (0,0)
-    ys = h - ys
-
-    colors = data.reshape(-1, 3)
-    color_strings = [f'rgb({r},{g},{b})' for r, g, b in colors]
-
-    # Apply user offset and initial offset
-    xs_trans = xs + initial_x_offset + offset_x * step
-    ys_trans = ys + offset_y * step
-
-    # Reflection about x=0: reflected_x = -original_x
-    xs_reflected = -xs_trans
-    ys_reflected = ys_trans
+def create_figure(offset_x, offset_y):
+    shift_x = offset_x * step + margin_from_axis
+    shift_y = offset_y * step
 
     fig = go.Figure()
 
-    # Mirror line at x=0
-    fig.add_trace(go.Scatter(
+    fig.add_trace(go.Scattergl(
         x=[0, 0],
-        y=[-100, 100],
+        y=[-200, 200],
         mode='lines',
         line=dict(color='gray', dash='dash'),
         name='Mirror Line'
     ))
 
-    # Original image
     fig.add_trace(go.Scattergl(
-        x=xs_trans,
-        y=ys_trans,
+        x=xs + shift_x,
+        y=ys + shift_y,
         mode='markers',
-        marker=dict(color=color_strings, size=4, opacity=1),
-        name='Original Image'
+        marker=dict(color=color_strings, size=5, opacity=1),
+        name='Original'
     ))
 
-    # Reflected image
     fig.add_trace(go.Scattergl(
-        x=xs_reflected,
-        y=ys_reflected,
+        x=-(xs + shift_x),
+        y=ys + shift_y,
         mode='markers',
-        marker=dict(color=color_strings, size=4, opacity=1),
-        name='Reflected Image'
+        marker=dict(color=color_strings, size=2, opacity=1),
+        name='Reflected'
     ))
 
     fig.update_layout(
-        xaxis=dict(scaleanchor="y"),
-        margin=dict(l=0, r=0, t=0, b=0)
+        xaxis=dict(scaleanchor="y", visible=False),
+        yaxis=dict(visible=False),
+        margin=dict(l=0, r=0, t=0, b=0),
+        showlegend=False
     )
 
     return fig
 
-dash.register_page(__name__, path='/challange5', name="Challenge 5", order=5)
+dash.register_page(__name__, path='/challenge5', name="Challenge 5", order=5)
 
 layout = html.Div([
     html.Div([
@@ -74,10 +63,11 @@ layout = html.Div([
         html.Button('down', id='down-nclicks', n_clicks=0),
         html.Button('left', id='left-nclicks', n_clicks=0),
         html.Button('right', id='right-nclicks', n_clicks=0),
-    ], style={'marginBottom': '20px'}),
+    ]),
     html.Div([
         html.H1("Challenge 5 - Plane Mirror Reflection"),
-        dcc.Graph(id="mirror")
+        html.Br(),
+        dcc.Graph(id="mirror", config={"displayModeBar": False})
     ])
 ])
 
@@ -89,6 +79,4 @@ layout = html.Div([
     Input('right-nclicks', 'n_clicks')
 )
 def update_figure(u, d, l, r):
-    offset_y = u - d
-    offset_x = r - l
-    return challenge_5_code(offset_x, offset_y)
+    return create_figure(r - l, u - d)
